8fdbc9570d82b16fae2bfea34b30dc1b66aa96ab
Abigail Grundy <abigailgrundy@Abigails-MBP.connect>
2019-02-27 09:03:08 +0000

    Question Counter Function

--- src/components/questionCounter.js
+++ src/components/questionCounter.js
@@ -1,16 +1,16 @@
 import React from 'react'
+import quizData from './quizData';
 
 class QuestionCounter extends React.Component {
 
     render() {
         return (
-        <div className = "row">
-
-           <p>Question 1 of 10</p>
-
+        <div className = "container-fluid">
+        Question {this.props.num} of {this.props.total} 
         </div>
-        )
+        );
     }
+
 }
 
 export default QuestionCounter;
