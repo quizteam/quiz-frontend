fb2177dac6a5415eedf93db2a532c12593747b32
Ruth Nassar <nassar_ruth@yahoo.co.uk>
2019-02-28 15:08:03 +0000

    trying toggling classnames

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -6,65 +6,32 @@ class Quiz extends React.Component {
  constructor(props) {
         super(props);
 
-        this.buttonRef = React.createRef()
-        this.state = {
-            color: "blue"
-        };
-
         this.onAnswerClicked = this.onAnswerClicked.bind(this);
-        //this.rightAnswer = this.rightAnswer.bind(this);
-        //this.wrongAnswer = this.wrongAnswer.bind(this);
-        //this.ButtonProgress = this.ButtonProgress.bind(this);
-    }
+    };
 
-    //rightAnswer = () => {
-      //  this.setState({ color: 'green' });
 
-    //wrongAnswer = () => {
-      //      this.setState({ color: 'red' });
 
-    //ButtonProgress (button) {
-      //  let rightAnswer = this.props.correct;
-        //rightAnswer = this.setState ({ color: 'green'});
-        //}
-        //wrongAnswer = this.setState ({ color: 'red'});
+    onAnswerClicked (answer) {
     
-
-    onAnswerClicked (answer){
-        //these two lines compare the id of the clicked answer
-        //to the correct answer, to see if the number is the same
         let answerGiven = answer.target.id;
         let correctAnswer = this.props.correct;
 
-        
-        //if it's the same, this code runs
         if (answerGiven == correctAnswer) {
             alert("You're right!"); 
             this.props.handleScoreIncrease();
-            
-            alert(this.state.color);
         
-
             this.props.handleNextQuestion();
-          
-            //this.ButtonProgress.rightAnswer();
-           
-           // this.rightAnswer();
 
-        //if the answer is incorrect, this code runs
         } else {
             alert ("That's not the right answer");
 
             this.props.handleNextQuestion();
-            
-        
-        }
+        };
     };
 
     render() {
 
         let  style= {
-            backgroundColor: this.state.color,
             margin: "10px",
             width: "400px",
             height: "50px",
@@ -81,6 +48,7 @@ class Quiz extends React.Component {
             <button 
             id = "0"
             type = "button" 
+            className="btn btn-success"
             ref = {this.buttonRef}
             style={style}
             onClick={this.onAnswerClicked} >
@@ -90,6 +58,7 @@ class Quiz extends React.Component {
             <div className = "row">
             <button 
             id = "1"
+            className="btn btn-danger"
             type = "button" 
             style={style}
             onClick={this.onAnswerClicked}>
@@ -99,6 +68,7 @@ class Quiz extends React.Component {
             <div className = "row">
             <button 
             id = "2"
+            className="btn btn-success"
             type = "button" 
             style={style}
             onClick={this.onAnswerClicked} >
@@ -108,6 +78,7 @@ class Quiz extends React.Component {
             <div className = "row">
             <button 
             id = "3"
+            className="btn btn-success"
             type = "button" 
             style={style}
             onClick={this.onAnswerClicked} >
