452c238eba4056fd184d37e3a717bbc484e1f53d
Abigail Grundy <abigailgrundy@Abigails-MBP.connect>
2019-03-03 18:52:34 +0000

    Styling added to Question Counter and Score Counter

--- src/components/questionCounter.js
+++ src/components/questionCounter.js
@@ -4,14 +4,16 @@ class QuestionCounter extends React.Component {
 
     render() {
 
-
         return (
-        <div className = "d-flex justify-content-center">
-        Question {this.props.num} of {this.props.total-1} 
+        <div className = "d-flex justify-content-center p-1">
+        <button type="button" class= 'btn btn-info' disabled>
+        Question {this.props.num} of {this.props.total-1}
+        </button>  
         </div>
-        );
+        )
     }
+    
 
-}
+};
 
 export default QuestionCounter;
